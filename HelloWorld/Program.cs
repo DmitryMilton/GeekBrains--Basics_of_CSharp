a97ed0d6470da77d8f7f7ee3c381fba1a8572409
DmitryMilton <dl.qbit@gmail.com>
2020-04-08 22:54:37 +0700

    Start studying! First project.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I want to learn C#!");
+            Console.ReadLine();
+        }
+    }
+}
