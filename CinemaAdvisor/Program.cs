636eec033472e69100a3694e2c35d8540b2a9fdf
DmitryMilton <dl.qbit@gmail.com>
2020-04-09 13:42:08 +0700

    Add "if else" to Cinema Adviser

--- CinemaAdvisor/Program.cs
+++ CinemaAdvisor/Program.cs
@@ -10,7 +10,7 @@ namespace CinemaAdvisor
             Console.WriteLine("Как вас зовут?");
             String name = Console.ReadLine();
             Console.WriteLine("Здравствуйте, " + name + "!");
-            Console.WriteLine("Фильм какого жанра вы хотели бы посмотреть?");
+            Console.WriteLine("Фильм какого жанра вы хотели бы посмотреть: комедия, драма, фантастика, триллер, боевик.");
             String genre = Console.ReadLine();
             Console.WriteLine("В таком случае рекомендую посмотреть вам следующие фильмы:");
             if (genre == "комедия")
@@ -20,12 +20,40 @@ namespace CinemaAdvisor
                 Console.WriteLine("Комедия 3");
                 Console.WriteLine("Комедия 4");
             }
+            else if (genre == "драма")
+            {
+                Console.WriteLine("Драма 1");
+                Console.WriteLine("Драма 2");
+                Console.WriteLine("Драма 3");
+                Console.WriteLine("Драма 4");
+            }
+            else if (genre == "фантастика")
+            {
+                Console.WriteLine("фантастика 1");
+                Console.WriteLine("фантастика 2");
+                Console.WriteLine("фантастика 3");
+                Console.WriteLine("фантастика 4");
+            }
+            else if ( genre.ToLower() == "триллер" )
+            {
+                Console.WriteLine("триллер 1");
+                Console.WriteLine("триллер 2");
+                Console.WriteLine("триллер 3");
+                Console.WriteLine("триллер 4");
+            }
+            else if (genre == "боевик")
+            {
+                Console.WriteLine("боевик 1");
+                Console.WriteLine("боевик 2");
+                Console.WriteLine("боевик 3");
+                Console.WriteLine("боевик 4");
+            }
             else
             {
-                Console.WriteLine("Фильм 1");
-                Console.WriteLine("Фильм 2");
-                Console.WriteLine("Фильм 3");
-                Console.WriteLine("Фильм 4");
+                Console.WriteLine("Другой жанр 1");
+                Console.WriteLine("Другой жанр 2");
+                Console.WriteLine("Другой жанр 3");
+                Console.WriteLine("Другой жанр 4");
             }
 
             Console.ReadLine();
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,18 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("I want to learn C#!");
-            Console.ReadLine();
+            //Console.WriteLine("I want to learn C#!");
+            //Console.ReadLine();
+
+            String a = Console.ReadLine();
+            String b = Console.ReadLine();
+
+            int num1 = Int32.Parse(a);
+            int num2 = Int32.Parse(b);
+
+            int result = num1 + num2;
+            Console.WriteLine(result);
+
         }
     }
 }
