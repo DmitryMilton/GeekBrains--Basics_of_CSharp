f68b5e12800e5ffebc7ed6348cf612a85224976e
DmitryMilton <dl.qbit@gmail.com>
2020-04-09 14:13:21 +0700

    Add ".ToLower" to genre

--- CinemaAdvisor/Program.cs
+++ CinemaAdvisor/Program.cs
@@ -13,21 +13,21 @@ namespace CinemaAdvisor
             Console.WriteLine("Фильм какого жанра вы хотели бы посмотреть: комедия, драма, фантастика, триллер, боевик.");
             String genre = Console.ReadLine();
             Console.WriteLine("В таком случае рекомендую посмотреть вам следующие фильмы:");
-            if (genre == "комедия")
+            if (genre.ToLower() == "комедия")
             {
                 Console.WriteLine("Комедия 1");
                 Console.WriteLine("Комедия 2");
                 Console.WriteLine("Комедия 3");
                 Console.WriteLine("Комедия 4");
             }
-            else if (genre == "драма")
+            else if (genre.ToLower() == "драма")
             {
                 Console.WriteLine("Драма 1");
                 Console.WriteLine("Драма 2");
                 Console.WriteLine("Драма 3");
                 Console.WriteLine("Драма 4");
             }
-            else if (genre == "фантастика")
+            else if (genre.ToLower() == "фантастика")
             {
                 Console.WriteLine("фантастика 1");
                 Console.WriteLine("фантастика 2");
@@ -41,7 +41,7 @@ namespace CinemaAdvisor
                 Console.WriteLine("триллер 3");
                 Console.WriteLine("триллер 4");
             }
-            else if (genre == "боевик")
+            else if (genre.ToLower() == "боевик")
             {
                 Console.WriteLine("боевик 1");
                 Console.WriteLine("боевик 2");
